5ca63a7913111cb0157b8b33be4b93e377089046
Long <long.nvb@kiotviet.com>
2024-02-09 02:18:08 +0700

    add overlay for search

--- src/app/core/components/app-header/app-header.component.ts
+++ src/app/core/components/app-header/app-header.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { RouterModule } from '@angular/router';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 
 @Component({
   selector: 'app-app-header',
   standalone: true,
   imports: [
     InputTextModule,
-    RouterModule
+    RouterModule,
+    OverlayPanelModule
   ],
   templateUrl: './app-header.component.html',
   styleUrl: './app-header.component.scss'

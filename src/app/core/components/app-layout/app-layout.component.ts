0e6c2719fa1a5c75b389a2b331fa3d08a8b3535d
Long <long.nvb@kiotviet.com>
2024-02-15 15:00:36 +0700

    add nav-bar and fix header

--- src/app/core/components/app-layout/app-layout.component.ts
+++ src/app/core/components/app-layout/app-layout.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppHeaderComponent } from '../app-header/app-header.component';
 import {MatInputModule} from '@angular/material/input';
+import { AppNavbarComponent } from '../app-navbar/app-navbar.component';
 
 @Component({
   selector: 'app-app-layout',
@@ -9,7 +10,8 @@ import {MatInputModule} from '@angular/material/input';
   imports: [
     RouterOutlet,
     AppHeaderComponent,
-    MatInputModule
+    MatInputModule,
+    AppNavbarComponent
   ],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss'

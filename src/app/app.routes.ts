f0541415a1a1cb70ed059cf8540262092cab2616
pearlde-bink <tuanbinhnguyenngoc@gmail.com>
2024-02-19 23:06:50 +0700

    add giayNam, phu kien, tre em, tin tuc, shopping cart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,12 @@ import { AppLayoutComponent } from './core/components/app-layout/app-layout.comp
 import { LoginComponent } from './features/auth/components/login/login.component';
 import { RegisterComponent } from './features/register/register.component';
 // import { IntroductionComponent } from './features/components/introduction/introduction.component';
-import { GiayNuComponent } from './features/components/giay-nu/giay-nu.component';
+// import { GiayNuComponent } from './features/components/giay-nu/giay-nu.component';
+// import { PhuKienComponent } from './features/components/phu-kien/phu-kien.component';
+// import { GiayNamComponent } from './features/components/giay-nam/giay-nam.component';
+// import { TreEmComponent } from './features/components/tre-em/tre-em.component';
+// import { NewsComponent } from './features/components/news/news.component';
+import { ShoppingCartComponent } from './features/components/shopping-cart/shopping-cart.component';
 
 export const routes: Routes = [
   {
@@ -37,15 +42,65 @@ export const routes: Routes = [
       //   component: IntroductionComponent
       // },
 
+      // {
+      //   path: '',
+      //   pathMatch: 'full',
+      //   redirectTo: '/giayNu'
+      // },
+      // {
+      //   path: 'giayNu',
+      //   component: GiayNuComponent
+      // },
+
+      // {
+      //   path: '',
+      //   pathMatch: 'full',
+      //   redirectTo: '/phuKien'
+      // },
+      // {
+      //   path: 'phuKien',
+      //   component: PhuKienComponent
+      // },
+
+      // {
+      //   path: '',
+      //   pathMatch: 'full',
+      //   redirectTo: '/giayNam'
+      // },
+      // {
+      //   path: 'giayNam',
+      //   component: GiayNamComponent
+      // },
+
+      // {
+      //   path: '',
+      //   pathMatch: 'full',
+      //   redirectTo: '/treEm'
+      // },
+      // {
+      //   path: 'treEm',
+      //   component: TreEmComponent
+      // },
+
+      // {
+      //   path: '',
+      //   pathMatch: 'full',
+      //   redirectTo: '/news'
+      // },
+      // {
+      //   path: 'news',
+      //   component: NewsComponent
+      // },
+
       {
         path: '',
         pathMatch: 'full',
-        redirectTo: '/giayNu'
+        redirectTo: '/shoppingCart'
       },
       {
-        path: 'giayNu',
-        component: GiayNuComponent
-      }
+        path: 'shoppingCart',
+        component: ShoppingCartComponent
+      },
     ]
   }
 ];
---
+++ src/app/features/components/tre-em/tre-em.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tre-em',
+  standalone: true,
+  imports: [],
+  templateUrl: './tre-em.component.html',
+  styleUrl: './tre-em.component.scss'
+})
+export class TreEmComponent {
+
+}

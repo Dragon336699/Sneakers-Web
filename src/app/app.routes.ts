56dd18e73a02cc5f700d514063b7aedc7c17f668
pearlde-bink <tuanbinhnguyenngoc@gmail.com>
2024-03-02 23:33:27 +0700

    them trang contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,27 +3,37 @@ import { AppLayoutComponent } from './core/components/app-layout/app-layout.comp
 import { HomeComponent } from './features/components/home/home.component';
 import { LoginComponent } from './features/auth/components/login/login.component';
 import { RegisterComponent } from './features/register/register.component';
+import { ContactComponent } from './features/components/contact/contact.component';
 
 export const routes: Routes = [
-    {
-        path: 'auth-login', component: LoginComponent
-    },
-    {
-        path: 'register', component: RegisterComponent
-    },
-    {
+  {
+    path: 'auth-login', component: LoginComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
     path: '',
     component: AppLayoutComponent,
     children: [
-        {
+      // {
+      //   path: '',
+      //   pathMatch: 'full',
+      //   redirectTo: '/Home'
+      // },
+      // {
+      //   path: 'Home',
+      //   component: HomeComponent
+      // },
+      {
         path: '',
         pathMatch: 'full',
-        redirectTo: '/Home'
-        },
-        {
-            path: 'Home',
-            component: HomeComponent
-        },
-]
-    }
+        redirectTo: '/contact'
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
+      },
+    ]
+  }
 ];

f73de4545f07007232f349138e6ba29e5323b3c7
Bảo Long <anhruoia1a1@gmail.com>
2024-04-11 00:17:19 +0700

    fix environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,5 @@
 export const environment = {
     production: false,
-<<<<<<< HEAD
-    apiUrl: 'http://localhost:8089/api/v1/'
-=======
     apiUrl: 'http://localhost:8089/api/v1/',
     apiImage: 'http://localhost:8089/api/v1/products/images/'
->>>>>>> d91dbe524ba101188d6b3e47bf1acf2ee1f4c7d9
 };
